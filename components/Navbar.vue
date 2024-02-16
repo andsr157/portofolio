30463be84627ed4360c6210ee761e77f83a357c9
andsr157 <andikasatrio159@gmail.com>
2024-02-16 23:39:12 +0700

    feat: implement digital clock on navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,25 @@
+<script setup lang="ts">
+const currentTime = ref(getCurrentTime())
+
+function getCurrentTime() {
+  const now = new Date()
+  const hours = now.getHours().toString().padStart(2, "0")
+  const minutes = now.getMinutes().toString().padStart(2, "0")
+  return `${hours}:${minutes}`
+}
+
+onMounted(() => {
+  setInterval(() => {
+    currentTime.value = getCurrentTime()
+  }, 1000)
+})
+</script>
+
 <template>
   <header>
     <div class="grid xl:grid-cols-3 py-6 lg:py-8 px-10 text-base items-center">
       <div class="text-left font-NeueHaas hidden lg:block">Andsr157</div>
-      <div class="text-center font-NeueHaas">20:34</div>
+      <div class="text-center font-NeueHaas">{{ currentTime }}</div>
       <div class="lg:flex justify-end gap-1 items-center hidden">
         <div class="text-left font-NeueHaas">Say hi</div>
         <div><Icon name="system-uicons:minus" size="2em" /></div>
