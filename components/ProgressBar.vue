7a64ead929f04401dac37a6f8d8e8ad4104632b1
andsr157 <andikasatrio159@gmail.com>
2024-02-16 23:39:29 +0700

    chore: delete unused code

--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -1,37 +1,3 @@
-<script setup lang="ts">
-import gsap from "gsap"
-// onMounted(() => {
-//   gsap.to("progress", {
-//     value: 100,
-//     ease: "none",
-//     end: "bottom bottom",
-//     scrollTrigger: { scrub: 0.3 },
-//     markers: true,
-//   })
-// })
-// onMounted(() => {
-//   let ctx = gsap.context(() => {
-//     // all your GSAP animation code here
-//     gsap.to(".progress-bar", {
-//       width: "100%",
-//       ease: "none",
-//       scrollTrigger: {
-//         trigger: "#main-layer",
-//         start: "top top",
-//         end: "bottom bottom",
-//         scrub: true,
-//         preventOverlaps: true,
-//         markers: true,
-//       },
-//     })
-//   })
-
-//   onBeforeUnmount(() => {
-//     ctx.revert()
-//   })
-// })
-</script>
-
 <template>
   <div
     class="relative grid overflow-hidden mx-3 xl:mx-10 px-5 xl:px-8 py-[10px] bg-black border-[1px] border-opacity-20 border-white rounded-[10px] font-NeueHaas"
