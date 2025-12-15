68583bb8bad489e0495792a9c5e7cffd0f3f48e2
andsr157 <andikasatrio159@gmail.com>
2025-12-16 00:51:58 +0700

    feat: update image layout and positioning in LinkSection component

--- components/home/LInkSection.vue
+++ components/home/LInkSection.vue
@@ -27,85 +27,53 @@ const toggleShow = (key: "webiste" | "landing" | "other") => {
         >
         <Transition name="fade">
           <div v-show="show.webiste">
-            <!-- Mobile: Absolute positioned within section bounds -->
-            <div class="lg:hidden">
-              <div class="absolute w-20 h-14 z-20 -top-16 left-4">
-                <NuxtImg
-                  src="/projects/img1.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 -top-20 right-4">
-                <NuxtImg
-                  src="/projects/img2.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 z-20 top-12 -right-2">
-                <NuxtImg
-                  src="/projects/img3.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 bottom-20 left-2">
-                <NuxtImg
-                  src="/projects/img5.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 bottom-8 right-8">
-                <NuxtImg
-                  src="/projects/img6.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 bottom-32 right-0">
-                <NuxtImg
-                  src="/projects/img9.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 z-20 top-4 -left-4 lg:top-12 lg:left-[16vw]"
+            >
+              <NuxtImg
+                src="/projects/img1.png"
+                class="w-full h-full object-cover"
+              />
             </div>
-            <!-- Desktop: Absolute positioned -->
-            <div class="hidden lg:block">
-              <div
-                class="absolute w-40 h-28 z-20 bg-red-300 top-12 left-[16vw]"
-              >
-                <NuxtImg
-                  src="/projects/img1.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 -top-28 right-[500px]">
-                <NuxtImg
-                  src="/projects/img2.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 z-20 bg-red-300 top-28 right-72">
-                <NuxtImg
-                  src="/projects/img3.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 bottom-72 right-1/2">
-                <NuxtImg
-                  src="/projects/img5.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 bottom-28 left-72">
-                <NuxtImg
-                  src="/projects/img6.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 bottom-20 right-96">
-                <NuxtImg
-                  src="/projects/img9.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 -top-6 right-12 lg:-top-28 lg:right-[500px]"
+            >
+              <NuxtImg
+                src="/projects/img2.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 z-20 top-10 -right-4 lg:top-28 lg:right-72"
+            >
+              <NuxtImg
+                src="/projects/img3.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-20 left-8 lg:top-auto lg:bottom-72 lg:right-1/2 lg:left-auto"
+            >
+              <NuxtImg
+                src="/projects/img5.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-24 right-4 lg:top-auto lg:bottom-28 lg:left-72 lg:right-auto"
+            >
+              <NuxtImg
+                src="/projects/img6.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-36 left-1/2 -translate-x-1/2 lg:translate-x-0 lg:top-auto lg:bottom-20 lg:right-96 lg:left-auto"
+            >
+              <NuxtImg
+                src="/projects/img9.png"
+                class="w-full h-full object-cover"
+              />
             </div>
           </div>
         </Transition>
@@ -121,71 +89,45 @@ const toggleShow = (key: "webiste" | "landing" | "other") => {
         >
         <Transition name="fade">
           <div v-show="show.landing">
-            <!-- Mobile: Absolute positioned within section bounds -->
-            <div class="lg:hidden">
-              <div class="absolute w-20 h-14 top-4 left-2">
-                <NuxtImg
-                  src="/projects/img4.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 -top-12 right-6">
-                <NuxtImg
-                  src="/projects/img11.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 z-20 top-10 -right-4">
-                <NuxtImg
-                  src="/projects/img8.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 bottom-16 left-0">
-                <NuxtImg
-                  src="/projects/img7.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 bottom-4 right-4">
-                <NuxtImg
-                  src="/projects/img10.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-4 -left-4 lg:top-[45%] lg:left-72"
+            >
+              <NuxtImg
+                src="/projects/img4.png"
+                class="w-full h-full object-cover"
+              />
             </div>
-            <!-- Desktop: Absolute positioned -->
-            <div class="hidden lg:block">
-              <div class="absolute w-40 h-28 bg-red-300 top-[45%] left-72">
-                <NuxtImg
-                  src="/projects/img4.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 -top-10 left-[450px]">
-                <NuxtImg
-                  src="/projects/img11.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 z-20 bg-red-300 top-64 right-80">
-                <NuxtImg
-                  src="/projects/img8.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 bottom-80 right-96">
-                <NuxtImg
-                  src="/projects/img7.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 bottom-11 left-[40%]">
-                <NuxtImg
-                  src="/projects/img10.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 -top-6 right-8 lg:-top-10 lg:left-[450px] lg:right-auto"
+            >
+              <NuxtImg
+                src="/projects/img11.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 z-20 top-10 -right-4 lg:top-64 lg:right-80 lg:left-auto"
+            >
+              <NuxtImg
+                src="/projects/img8.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-20 right-8 lg:top-auto lg:bottom-80 lg:right-96 lg:left-auto"
+            >
+              <NuxtImg
+                src="/projects/img7.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-28 left-4 lg:top-auto lg:bottom-11 lg:left-[40%] lg:right-auto"
+            >
+              <NuxtImg
+                src="/projects/img10.png"
+                class="w-full h-full object-cover"
+              />
             </div>
           </div>
         </Transition>
@@ -201,93 +143,58 @@ const toggleShow = (key: "webiste" | "landing" | "other") => {
         >
         <Transition name="fade">
           <div v-show="show.other">
-            <!-- Mobile: Absolute positioned within section bounds -->
-            <div class="lg:hidden">
-              <div class="absolute w-20 h-14 top-8 left-0">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img12.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 -top-8 left-1/2 -translate-x-1/2">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img13.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 top-4 -right-2">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img14.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 bottom-20 right-2">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img16.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
-              <div class="absolute w-20 h-14 bottom-6 left-4">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img15.png"
-                  class="w-full h-full object-cover rounded"
-                />
-              </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-4 -left-4 lg:top-[70%] lg:left-64"
+            >
+              <NuxtImg
+                format="webp"
+                quality="60"
+                src="/projects/img12.png"
+                class="w-full h-full object-cover"
+              />
             </div>
-            <!-- Desktop: Absolute positioned -->
-            <div class="hidden lg:block">
-              <div class="absolute w-40 h-28 bg-red-300 top-[70%] left-64">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img12.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 top-[50%] left-[40vw]">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img13.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 top-[80%] left-[50vw]">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img14.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div
-                class="absolute w-40 h-28 bg-red-300 bottom-80 right-[400px]"
-              >
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img16.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
-              <div class="absolute w-40 h-28 bg-red-300 bottom-72 right-[60vw]">
-                <NuxtImg
-                  format="webp"
-                  quality="60"
-                  src="/projects/img15.png"
-                  class="w-full h-full object-cover"
-                />
-              </div>
+
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 -top-6 right-8 lg:translate-x-0 lg:top-[50%] lg:left-[40vw]"
+            >
+              <NuxtImg
+                format="webp"
+                quality="60"
+                src="/projects/img13.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-10 -right-4 lg:translate-x-0 lg:top-[80%] lg:left-[50vw] lg:right-auto"
+            >
+              <NuxtImg
+                format="webp"
+                quality="60"
+                src="/projects/img14.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-20 right-4 lg:top-auto lg:bottom-80 lg:right-[400px]"
+            >
+              <NuxtImg
+                format="webp"
+                quality="60"
+                src="/projects/img16.png"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div
+              class="absolute w-16 h-12 lg:w-40 lg:h-28 top-28 left-4 lg:top-auto lg:bottom-72 lg:right-[60vw] lg:left-auto"
+            >
+              <NuxtImg
+                format="webp"
+                quality="60"
+                src="/projects/img15.png"
+                class="w-full h-full object-cover"
+              />
             </div>
           </div>
         </Transition>
