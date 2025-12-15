65657a668d0fd8d8dcfd0b460971abf6315b21e7
andsr157 <andikasatrio159@gmail.com>
2025-12-16 00:35:06 +0700

    fix: adjust button layout in ProgressBar component

--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -5,9 +5,9 @@
     <div class="h-full absolute progress-bar bg-[#202020] w-0" />
     <div class="flex justify-between items-center relative">
       <div class="text-[16px]">Personal portofolio</div>
-      <div class="flex justify-end">
+      <div class="w-36">
         <a href="mailto:andikasatrio159@gmail.com" target="_blank">
-          <Button title="Contact me" custom-class="w-36" />
+          <Button title="Contact me" />
         </a>
       </div>
     </div>
